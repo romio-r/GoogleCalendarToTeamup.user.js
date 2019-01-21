95e8c083e8403fda72c85bc3127638f0843e6563
romio-r <pryshlyak.roman@gmail.com>
2019-01-21 11:51:10 +0200

    Updated selectors according to new design

--- GoogleCalendarToTeamup.user.js
+++ GoogleCalendarToTeamup.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Google calendar to Teamup teamup.com
 // @namespace    http://tampermonkey.net/
-// @version      0.6
+// @version      0.7
 // @updateURL    https://github.com/romio-r/GoogleCalendarToTeamup.user.js/raw/master/GoogleCalendarToTeamup.user.js
 // @description  Adds button to add Google calendar event to Teamup teamup.com calendar
 // @author       romio-r
@@ -28,7 +28,7 @@ var tu_debug = false;
                 }
 
                 // if no opened calendar event details popup
-                if($(".osAuJb span").length == 0){
+                if($(".pdqVLc span").length == 0){
                    if(tu_debug){
                        console.log('No opened calendar event details popup');
                    }
@@ -41,7 +41,10 @@ var tu_debug = false;
                 }
 
                 // get name of event
-                var title = $(".osAuJb span").text();
+                var title = $(".JAPzS span[role='heading']").text();
+                if(tu_debug){
+                    console.log('Title: ' + title);
+                }
 
                 //get username
                 var user = $("a.gb_b.gb_R").attr('aria-label');
@@ -64,7 +67,7 @@ var tu_debug = false;
                 }
 
                 //get start and end time
-                var startEndTimeRaw = $("div.NI2kfb div div.DN1TJ span").first().text();
+                var startEndTimeRaw = $(".DN1TJ.fX8Pqc.CyPPBf").children('span').eq(1).text();
                 var startEndTime = startEndTimeRaw.split(" – ");
                 if(tu_debug){
                     console.log('startEndTimeRaw: ' + startEndTimeRaw);
@@ -72,11 +75,12 @@ var tu_debug = false;
                 }
 
                 // get date
-                var date = $("div.kMp0We.OcVpRe>div.NI2kfb .Q3pZ0e .JAPzS").first().text();
+                var date = $(".DN1TJ.fX8Pqc.CyPPBf").first().text();
+
                 if(tu_debug){
                     console.log('dateString: ' + date);
                 }
-                date = date.split(", ")[1].split(" ");
+                date = date.split(", ")[1].split("⋅")[0].split(" ");
                 if(tu_debug){
                     console.log('date after split: ' + date);
                 }
@@ -104,7 +108,7 @@ var tu_debug = false;
                 if(($(".osAuJb span").length == 0) && tu_debug){
                     console.log('No place to append integration');
                 }
-                $(".osAuJb").append("<div class=\"gctotsiyowise\" style=\"position: absolute; top: 12px; left: 20px; \">" + a + "</div>");
+                $(".pdqVLc").append("<div class=\"gctotsiyowise\" style=\"position: absolute; top: 12px; left: 20px; \">" + a + "</div>");
             }, 500);
     });
 })();
