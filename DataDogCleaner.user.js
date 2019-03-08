32adce1b17e74990e35e52e72cfea4a44eba70e0
Yaroslav Martsynyuk <yaro@teacherspayteachers.com>
2019-03-08 17:54:06 +0200

    Update DataDogCleaner.user.js

--- DataDogCleaner.user.js
+++ DataDogCleaner.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         DataDogCleaner
 // @namespace    http://tampermonkey.net/
-// @version      0.9
+// @version      0.91
 // @updateURL    https://github.com/romio-r/GoogleCalendarToTeamup.user.js/raw/master/DataDogCleaner.user.js
 // @description  Hides header at TV
 // @author       You
